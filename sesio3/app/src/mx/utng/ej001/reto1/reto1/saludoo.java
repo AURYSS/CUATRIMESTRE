e8f35c54e2b86ca7b8ce16dd395dbb892811db49
Aurora Robelo <114744861+AURYSS@users.noreply.github.com>
2024-04-18 18:48:42 -0600

    Add files via upload

---
+++ sesio3/app/src/mx/utng/ej001/reto1/reto1/saludoo.java
@@ -0,0 +1,7 @@
+public class saludoo {
+    public static void main(String[] args){
+        System.out.println(x:"hola tacho");
+        
+
+    }
+}
---
+++ sesio3/app/src/mx/utng/ej001/reto1/saludo.java
@@ -0,0 +1,6 @@
+public class saludo {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in)
+    }
+    
+}
