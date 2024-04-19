c08700df77151f043ac6b500877add6e76cf33e4
Aurora Robelo <114744861+AURYSS@users.noreply.github.com>
2024-04-18 18:59:50 -0600

    Add files via upload

---
+++ sesion27/sesion27/Sesion27/src/mx/utng/s27/reto1/Hilo2.java
@@ -0,0 +1,23 @@
+package mx.utng.s27.reto1;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hilo2 extends Thread{
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("Hilo2: Sigo en ejecución ");
+            try {
+                TimeUnit.MILLISECONDS.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            
+        }
+    }
+    
+}
+/* @name: cecilia aurora robelo hernandez
+ * @group: GDS0624
+ */
